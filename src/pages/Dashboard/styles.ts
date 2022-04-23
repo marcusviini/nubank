c7aef7549212c34dc6099210c8a325b4b1cef613
marcusviini <marcustere28@gmail.com>
2022-04-23 01:38:45 -0300

    refactor: change background color

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -2,7 +2,5 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: red;
-  align-self: stretch;
-  text-align: center;
+  background-color: #8B10AE;
 `;
